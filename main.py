b659e08cecf25970ff6301ea7533b4ee21f1a80d
Siguhau <sigurd.haua@gmail.com>
2024-05-15 11:31:12 +0200

    Add: all locations

--- main.py
+++ main.py
@@ -1,11 +1,62 @@
-from analyzer import analyze_results
-from gpt_api import ask_test
-from utils import *
-from logger import log
+from utils import run_tasks
 
+#scarf
+print("scarf")
+scarf_path = r"C:\Users\sigur\Downloads\master-applications\scarf-beta"
+run_tasks(scarf_path, "scarf-beta", ["php"])
 
-path = r"C:\Users\sigur\Downloads\master-applications\scarf-beta"
-php_files = get_files_of_given_type_in_folder(path, "php")
+#awcm
+print("awcm")
+awcm_path = r"C:\Users\sigur\Downloads\master-applications\AWCM v2.2 final\awcm"
+run_tasks(awcm_path, "awcm", ["php", "js"])
 
+#events_lister
+print("events_lister")
+events_lister_path = r"C:\Users\sigur\Downloads\master-applications\Basic-php-events-lister2.03\events2"
+run_tasks(events_lister_path, "events_lister", ["php"])
 
-run_tasks(path, "scarf-beta-test-4", ["php"])
+#bwapp
+print("bwapp")
+bwapp_path = r"C:\Users\sigur\Downloads\master-applications\bWAPPv2.2\bWAPP"
+run_tasks(bwapp_path, "bwapp", ["php"])
+
+#dvwa
+print("dvwa")
+dvwa_path = r"C:\Users\sigur\Downloads\master-applications\DVWA-master\DVWA-master"
+run_tasks(dvwa_path, "dvwa", ["php"])
+
+#jsforum
+print("jsforum")
+jsforum_path = r"C:\Users\sigur\Downloads\master-applications\JsForum0.01beta"
+run_tasks(jsforum_path, "jsforum", ["jsp", "java"])
+
+#mybb
+print("mybb")
+mybb_path = r"C:\Users\sigur\Downloads\master-applications\mybb_1607\Upload"
+run_tasks(mybb_path, "mybb", ["php"])
+
+#mybloggie
+print("mybloggie")
+mybloggie_path = r"C:\Users\sigur\Downloads\master-applications\mybloggie214"
+run_tasks(mybloggie_path, "mybloggie", ["php"])
+
+#phpns
+print("phpns")
+phpns_path = r"C:\Users\sigur\Downloads\master-applications\phpns211"
+run_tasks(phpns_path, "phpns", ["php"])
+
+#phpoll
+print("phpoll")
+phpoll_path = r"C:\Users\sigur\Downloads\master-applications\phpoll-097beta"
+run_tasks(phpoll_path, "phpoll", ["php"])
+
+#online_store
+print("online_store")
+online_store_path = r"C:\Users\sigur\Downloads\master-applications\Shopping-web-Jsp-Servlet-master\Shopping-web-Jsp-Servlet-master"
+run_tasks(online_store_path, "online_store", ["jsp", "java"])
+
+#wackopicko
+print("wackopicko")
+wackopicko_path = r"C:\Users\sigur\Downloads\master-applications\WackoPicko-master"
+run_tasks(wackopicko_path, "wackopicko", ["php"])
+
