a5cbbec6dc79233e1c52d45865e98e0fffaab68b
Siguhau <sigurd.haua@gmail.com>
2024-05-27 19:33:33 +0200

    Add: cot analysis

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 def starts_with_yes_no_or_other(word):
     word = word.lower()
@@ -118,3 +119,132 @@ def analyze_results(file_path):
             'file': file_path,
         }
     save_to_file(results, "analyze_results.jsonl")
+
+def cot_analyze_single_result(data, file_path):
+    cwe_list = [
+    'CWE-22', 'CWE-23', 'CWE-35', 'CWE-59', 'CWE-200', 'CWE-201', 'CWE-219', 'CWE-275', 'CWE-276',
+    'CWE-284','CWE-285', 'CWE-352', 'CWE-359', 'CWE-377', 'CWE-402', 'CWE-425', 'CWE-441', 'CWE-497',
+    'CWE-538', 'CWE-540', 'CWE-548', 'CWE-552', 'CWE-566', 'CWE-601', 'CWE-639', 'CWE-651', 'CWE-668',
+    'CWE-706', 'CWE-862', 'CWE-863', 'CWE-913', 'CWE-922', 'CWE-1275'
+    ]
+    vulnerable_files = []
+
+    id = data['id']
+    prompt_type = data['prompt_type']
+    file = data['file']
+    answer = data['response']['choices'][0]['message']['content']
+
+    # Define the order of answers to search for
+    answer_types = ["Final Answer", "Final Conclusion", "Answer", "Conclusion", "Summary"]
+    
+    # Function to search for a yes or no in the given answer
+    def search_for_yes_no(answer):
+        if 'yes' in answer.lower():
+            return 'yes'
+        elif 'no' in answer.lower():
+            return 'no'
+        else:
+            return None
+    
+    # Perform the check
+    # Look for answer patter
+    orig_answer = answer
+    answer = answer.lower()
+    for answer_type in answer_types:
+        answer_type = answer_type.lower()
+        # does answer contain the answer_type
+        if answer_type not in answer:
+            continue
+
+        #get the answer after the first apperance of the answer type
+        final_answer = answer.split(answer_type, 1)[1]
+        #check if the answer contains yes or no
+        yes_no = search_for_yes_no(final_answer)
+        if yes_no == 'yes':
+            cwe_in_answer = contains_cwe(final_answer, cwe_list)
+            if len(cwe_in_answer) == 0:
+                save_dict = {
+                    'id': id,
+                    'file': file,
+                    'prompt_type': prompt_type,
+                    'short_answer': 'yes',
+                    'cwe_in_answer': 'None',
+                    'answer': orig_answer
+                }
+                save_to_file(save_dict, file_path)
+            else:
+                save_dict = {
+                    'id': id,
+                    'file': file,
+                    'prompt_type': prompt_type,
+                    'short_answer': 'yes',
+                    'cwe_in_answer': cwe_in_answer,
+                    'answer': orig_answer
+                }
+                vulnerable_files.append(file)
+                save_to_file(save_dict, file_path)
+        if yes_no == 'no':
+            save_dict = {
+                'id': id,
+                'file': file,
+                'prompt_type': prompt_type,
+                'short_answer': 'no',
+                'cwe_in_answer': 'None',
+                'answer': orig_answer
+            }
+            save_to_file(save_dict, file_path)
+        
+        if yes_no == None:
+            save_dict = {
+                'id': id,
+                'file': file,
+                'prompt_type': prompt_type,
+                'short_answer': 'other',
+                'cwe_in_answer': 'None',
+                'answer': orig_answer
+            }
+            save_to_file(save_dict, file_path)
+
+    return vulnerable_files
+
+def cot_analyze_results(file_path):
+    # create the set vulnerable_files
+    vulnerable_files = set()
+    with open(file_path) as f:
+        for line in f:
+            data = json.loads(line)
+            vulnerable_files.update(cot_analyze_single_result(data, file_path))
+    # save vulnerable files
+    save_to_file({'vulnerable_files': list(vulnerable_files)}, "cot_vulnerable_files.jsonl")
+
+    with open("analyze/analyze/"+ file_path, "r") as f:
+        inc = 0
+        yes = 0
+        no = 0
+        other = 0
+        yes_but_no_cwe = 0
+        for line in f:
+            inc += 1
+            data = json.loads(line)
+            short_answer = data['short_answer']
+            if short_answer == 'yes':
+                yes += 1
+                cwe_in_answer = data['cwe_in_answer']
+                if cwe_in_answer == 'None':
+                    yes_but_no_cwe += 1
+            elif short_answer == 'no':
+                no += 1
+            else:
+                other += 1
+
+        results = {
+            'total': inc,
+            'yes': yes,
+            'no': no,
+            'other': other,
+            'yes_but_no_cwe': yes_but_no_cwe,
+            'file': file_path,
+        }
+    save_to_file(results, "cot_analyze_results.jsonl")
+
+            
--- main.py
+++ main.py
@@ -1,62 +1,76 @@
 from utils import run_tasks
-
+from analyzer import cot_analyze_results
 #scarf
 #print("scarf")
-#scarf_path = r"C:\Users\sigur\Downloads\master-applications\scarf-beta"
+scarf_path = r"C:\Users\sigur\Downloads\master-applications\scarf-beta"
 #run_tasks(scarf_path, "scarf-beta", ["php"])
+cot_analyze_results("results/scarf-beta/simple_cot.jsonl")
+
+
 
 #awcm
 # print("awcm")
-# awcm_path = r"C:\Users\sigur\Downloads\master-applications\AWCM v2.2 final\awcm"
+awcm_path = r"C:\Users\sigur\Downloads\master-applications\AWCM v2.2 final\awcm"
 # run_tasks(awcm_path, "awcm", ["php", "js"])
+cot_analyze_results("results/awcm/simple_cot.jsonl")
 
 #events_lister
 # print("events_lister")
-# events_lister_path = r"C:\Users\sigur\Downloads\master-applications\Basic-php-events-lister2.03\events2"
+events_lister_path = r"C:\Users\sigur\Downloads\master-applications\Basic-php-events-lister2.03\events2"
 # run_tasks(events_lister_path, "events_lister", ["php"])
+cot_analyze_results("results/events_lister/simple_cot.jsonl")
 
 #bwapp
 # print("bwapp")
-# bwapp_path = r"C:\Users\sigur\Downloads\master-applications\bWAPPv2.2\bWAPP"
+bwapp_path = r"C:\Users\sigur\Downloads\master-applications\bWAPPv2.2\bWAPP"
 # run_tasks(bwapp_path, "bwapp", ["php"])
+cot_analyze_results("results/bwapp/simple_cot.jsonl")
 
 #dvwa
 # print("dvwa")
-# dvwa_path = r"C:\Users\sigur\Downloads\master-applications\DVWA-master\DVWA-master"
+dvwa_path = r"C:\Users\sigur\Downloads\master-applications\DVWA-master\DVWA-master"
 # run_tasks(dvwa_path, "dvwa", ["php"])
+cot_analyze_results("results/dvwa/simple_cot.jsonl")
 
 #jsforum
 # print("jsforum")
-# jsforum_path = r"C:\Users\sigur\Downloads\master-applications\JsForum0.01beta"
+jsforum_path = r"C:\Users\sigur\Downloads\master-applications\JsForum0.01beta"
 # run_tasks(jsforum_path, "jsforum", ["jsp", "java"])
+cot_analyze_results("results/jsforum/simple_cot.jsonl")
 
 #mybb
 # print("mybb")
-# mybb_path = r"C:\Users\sigur\Downloads\master-applications\mybb_1607\Upload"
+mybb_path = r"C:\Users\sigur\Downloads\master-applications\mybb_1607\Upload"
 # run_tasks(mybb_path, "mybb", ["php"])
+cot_analyze_results("results/mybb/simple_cot.jsonl")
 
 #mybloggie
-print("mybloggie")
+#print("mybloggie")
 mybloggie_path = r"C:\Users\sigur\Downloads\master-applications\mybloggie214"
-run_tasks(mybloggie_path, "mybloggie", ["php"])
+#run_tasks(mybloggie_path, "mybloggie", ["php"])
+cot_analyze_results("results/mybloggie/simple_cot.jsonl")
 
 #phpns
-print("phpns")
+#print("phpns")
 phpns_path = r"C:\Users\sigur\Downloads\master-applications\phpns211"
-run_tasks(phpns_path, "phpns", ["php"])
+#run_tasks(phpns_path, "phpns", ["php"])
+cot_analyze_results("results/phpns/simple_cot.jsonl")
 
 #phpoll
-print("phpoll")
+#print("phpoll")
 phpoll_path = r"C:\Users\sigur\Downloads\master-applications\phpoll-097beta"
-run_tasks(phpoll_path, "phpoll", ["php"])
+#run_tasks(phpoll_path, "phpoll", ["php"])
+cot_analyze_results("results/phpoll/simple_cot.jsonl")
 
 #online_store
-print("online_store")
+#print("online_store")
 online_store_path = r"C:\Users\sigur\Downloads\master-applications\Shopping-web-Jsp-Servlet-master\Shopping-web-Jsp-Servlet-master"
-run_tasks(online_store_path, "online_store", ["jsp", "java"])
+#run_tasks(online_store_path, "online_store", ["jsp", "java"])
+cot_analyze_results("results/online_store/simple_cot.jsonl")
 
 #wackopicko
-print("wackopicko")
+#print("wackopicko")
 wackopicko_path = r"C:\Users\sigur\Downloads\master-applications\WackoPicko-master"
-run_tasks(wackopicko_path, "wackopicko", ["php"])
+#run_tasks(wackopicko_path, "wackopicko", ["php"])
+cot_analyze_results("results/wackopicko/simple_cot.jsonl")
 
